97c35e48ee0bb5a00b938b5dd187e9d0f0a8a470
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-19 13:46:45 +0600

    load single service data

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 console.log(process.env.DB_USER)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w8gsdns.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -33,6 +33,20 @@ async function run() {
     app.get('/services', async(req,res)=>{
         const cursor= servicesCollection.find();
         const result= await cursor.toArray();
+        res.send(result)
+    } )
+
+    app.get('/services/:id', async(req,res) =>{
+        const id= req.params.id;
+        const query= {_id : new ObjectId (id) }
+    
+            const options = {
+      // Sort matched documents in descending order by rating
+      
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: {  title: 1, price:1, service_id: 1 },
+    };
+        const result= await servicesCollection.findOne(query, options );
         res.send(result)
     } )
 
